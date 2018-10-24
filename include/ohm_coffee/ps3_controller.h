359712503661ea1a9e33f0edbc3eb3741ad06927
Your Name <you@example.com>
2018-10-24 10:32:02 +0000

    -- initial commit from tegra

---
+++ include/ohm_coffee/ps3_controller.h
@@ -0,0 +1,44 @@
+// note on plain values:
+// buttons are either 0 or 1
+// button axes go from 0 to -1
+// stick axes go from 0 to +/-1
+
+#define BUTTON_SELECT            0
+#define BUTTON_STICK_LEFT        1
+#define BUTTON_STICK_RIGHT       2
+#define BUTTON_START             3
+#define BUTTON_CROSS_UP          4
+#define BUTTON_CROSS_RIGHT       5
+#define BUTTON_CROSS_DOWN        6
+#define BUTTON_CROSS_LEFT        7
+#define BUTTON_REAR_LEFT_2       8
+#define BUTTON_REAR_RIGHT_2      9
+#define BUTTON_REAR_LEFT_1       10
+#define BUTTON_REAR_RIGHT_1      11
+#define BUTTON_ACTION_TRIANGLE   12
+#define BUTTON_ACTION_CIRCLE     13
+#define BUTTON_ACTION_CROSS      14
+#define BUTTON_ACTION_SQUARE     15
+#define BUTTON_PAIRING           16
+
+
+#define STICK_LEFT_HORIZONTAL    	0
+#define STICK_LEFT_VERTICAL      	1
+#define AXIS_STICK_RIGHT_LEFTWARDS   	2
+#define AXIS_STICK_RIGHT_UPWARDS     	3
+#define AXIS_BUTTON_CROSS_UP         	4
+#define AXIS_BUTTON_CROSS_RIGHT      	5
+#define AXIS_BUTTON_CROSS_DOWN       	6
+#define AXIS_BUTTON_CROSS_LEFT       	7
+#define AXIS_BUTTON_REAR_LEFT_2      	8
+#define AXIS_BUTTON_REAR_RIGHT_2     	9
+#define AXIS_BUTTON_REAR_LEFT_1      	10
+#define AXIS_BUTTON_REAR_RIGHT_1     	11
+#define AXIS_BUTTON_TRIANGLE  		12
+#define AXIS_BUTTON_ACTION_CIRCLE    	13
+#define AXIS_BUTTON_ACTION_CROSS     	14
+#define AXIS_BUTTON_ACTION_SQUARE   	15
+#define AXIS_ACCELEROMETER_LEFT      	16
+#define AXIS_ACCELEROMETER_FORWARD   	17
+#define AXIS_ACCELEROMETER_UP        	18
+#define AXIS_GYRO_YAW                	19
---
+++ src/ps3_controller.cpp
@@ -0,0 +1,61 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include "ohm_coffee/ps3_controller.h"
+
+ros::Publisher pubTwist;
+
+void ps3ControllerCallback(const sensor_msgs::Joy& msg)
+{
+    
+    //Check ob Controller in der Hand über Bewegungssensoren
+    
+/*
+	bool enabled;
+
+        if(msg.axes.AXIS_ACCELEROMETER_LEFT < 0.2 && msg.axes.AXIS_ACCELEROMETER_LEFT > -0.2
+	|| msg.axes.AXIS_ACCELEROMETER_FORWARD < 0.2 && msg.axes.AXIS_ACCELEROMETER_FORWARD > -0.2 )
+		enabled = false;
+	else
+		enabled = true;
+	
+*/
+	geometry_msgs::Twist velocity;
+
+	// von struct ps3Val zu velocity.bla umwandeln 
+	velocity.angular.z = msg.axes[AXIS_STICK_RIGHT_LEFTWARDS];
+	velocity.linear.x  = msg.axes[STICK_LEFT_VERTICAL];
+
+	std::cout << "angular: " << velocity.angular.z << std::endl; 
+	std::cout << "linear:  " << velocity.linear.x  << std::endl; 
+
+	pubTwist.publish(velocity);
+	
+
+
+	// Umrechnung von Controller Input auf Roboter Output und abspeichern
+	// Voller Stickausschlag bedeutet 1 m/s, Verlauf lin/exp?
+	//ps3Val.angular = 1 * msg.axes.0 * int(enabled);
+	//ps3Val.linear = 1 * msg.axes.3 * int(enabled);
+
+
+	std::cout << __PRETTY_FUNCTION__ << std::endl; 
+}
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "ps3_controller");
+
+	ros::NodeHandle nh;
+	
+
+
+	ros::Subscriber subJoy  = nh.subscribe("joy", 1, ps3ControllerCallback);
+	pubTwist = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1);
+
+	
+
+	ros::spin(); 
+
+}

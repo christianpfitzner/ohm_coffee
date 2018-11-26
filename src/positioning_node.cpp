0d606ef1af3364d8e56c88d393240d414b147ebc
Lz310 <017663047922@o2online.de>
2018-11-27 00:44:10 +0100

    new node for positioning

---
+++ src/positioning_node.cpp
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+
+
+//ros::Publisher pubParam;
+
+int main(int argc, char** argv)
+{
+
+	ros::init(argc, argv, "positioning_node");
+	int var = 0;
+	std::string s;
+	
+
+	ros::NodeHandle prvNh("~");
+
+	prvNh.param<int>("x", var, 4711);
+	std::cout << " read param x: " << var << std::endl;
+
+        prvNh.param<int>("y", var, 4711);
+	std::cout << " read param y: " << var << std::endl;
+
+	prvNh.param<std::string>("testString", s, "nix");
+	std::cout << " read string :" << s << std::endl;
+
+
+
+		
+
+	//pubParam = nh.advertise<std_msgs::Int8>("yaml_Parameter", 1);	
+
+	//pubParam.publish(msg);
+}

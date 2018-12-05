7e9f8d6598c726edcab4315baf527945671c377a
moessneran <moessneran59696@th-nuernberg.de>
2018-12-05 12:33:10 +0100

    include node and publisher function

--- coffee_th_server/simple_server_main.cpp
+++ coffee_th_server/simple_server_main.cpp
@@ -3,29 +3,37 @@
 #include <string>
 #include <iostream>
 
-int main (int argc, int argv [])
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+int main(int argc, char** argv)
 {
-try
-{
-ServerSocket server(7850);
-while(true)
-{
-ServerSocket new_sock;
-server.accept(new_sock);
-try
-{ while(true)
-{
-std::string data;
-new_sock >> data;
+	std::cout << "Hallo Wo" << std::endl;
+	ros::init (argc, argv,  "androidID");
+	ros::NodeHandle nhServer;
+	ros::Publisher android_pub = nhServer.advertise<std_msgs::String>("Android" , 1000);
+	try
+	{
+		ServerSocket server(7850);
+	while(true)
+	{		
+	ServerSocket new_sock;
+	server.accept(new_sock);
+		try
+		{ while(true)
+		{
+		std_msgs::String msg;
+		std::string data;
+		new_sock >> data;
 std::cout << data << std::endl;
+		msg.data = data;
+		android_pub.publish(msg);
+		}
+		}
+		catch (SocketException&) {}
 
-new_sock << data;
-}
-}
-catch (SocketException&) {}
-
-}
-}
+		}
+		}
 catch (SocketException & e)
 {
 std::cout << "Error" << e.description () << "\nExiting. \n";

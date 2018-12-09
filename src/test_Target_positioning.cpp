09526ec166498e4bedb14b189ef959d35bb43c3e
Lz310 <017663047922@o2online.de>
2018-12-09 21:30:54 +0100

    tests..

--- src/test_Target_positioning.cpp
+++ src/test_Target_positioning.cpp
@@ -1,12 +1,17 @@
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
 #include "std_msgs/String.h"
 
-ros::Publisher targetPositions;
+
+
+ros::Publisher targetPos;
 
 void androidIDCallback(const std_msgs::String::ConstPtr& msg)
 {
 	ROS_INFO("Android ID: [%s]", msg->data.c_str());
+
+
 /*	
 	if (ID == 123)
 	{
@@ -14,7 +19,7 @@ void androidIDCallback(const std_msgs::String::ConstPtr& msg)
 	y = 
 	}
 
-	if (ID == 456)
+	if (ID == 456)r
 	{
 	x = 
 	y = 
@@ -26,7 +31,7 @@ void androidIDCallback(const std_msgs::String::ConstPtr& msg)
 	y = 
 	}
 */
-	//targetPositions.publish(hier noch daten);
+	//targetPositions.publish();
 }
 
 
@@ -47,6 +52,7 @@ int main(int argc, char** argv)
 
 	ros::NodeHandle prvNh("~");
 
+
 	// parameters position 1
 
 	prvNh.param<int>("x1", target_x, 0);
@@ -55,30 +61,31 @@ int main(int argc, char** argv)
 	prvNh.param<int>("yaw1", target_z, 0);
 
 
-
+/*
 	// parameters position 2
 
 	prvNh.param<int>("x2", target_x, 0);
-	std::cout << "testParam2 :" << target_x << std::endl;
 	prvNh.param<int>("y2", target_y, 0);
 	prvNh.param<int>("yaw2", target_z, 0);
-
 
 
 	// parameters position 3
 
 	prvNh.param<int>("x3", target_x, 0);
-	std::cout << "testParam3 :" << target_x << std::endl;
 	prvNh.param<int>("y3", target_y, 0);
 	prvNh.param<int>("yaw3", target_z, 0);
 
-
+*/
 
 	ros::Subscriber sub = prvNh.subscribe("Android", 1, androidIDCallback);
-	//targetPositions = prvNh.advertise<std_msgs::Int8>("hier noch Zielpfad", 1);
 
 
-	//ros::spin(); 
+/*
+	targetPos = prvNh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal geometry_msgs/PoseStamped '{ header: { frame_id: map }, pose: { position: { x: 1, y: 1 , z: 0}, orientation: { x: 0, y: 0, z: 0, w: 1 }}}'", 1);
+*/
+	//targetPositions.publish(PoseStamped);
+
+	ros::spin(); 
 
 		
 }

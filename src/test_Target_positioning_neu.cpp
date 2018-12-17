fac5ce4435814c441cf1d24b5c3241c028d252b8
Lz310 <017663047922@o2online.de>
2018-12-17 15:16:34 +0100

    improved code

--- src/test_Target_positioning_neu.cpp
+++ src/test_Target_positioning_neu.cpp
@@ -11,25 +11,43 @@
 
 void androidIDCallback(const std_msgs::String::ConstPtr& msg)
 {
+	ros::NodeHandle prvNh("~");
+	ros::NodeHandle nhServer;
+	ros::Publisher targetPos;
+
+	XmlRpc::XmlRpcValue param_list;
+	geometry_msgs::PoseStamped Target;
+
+	std::string name;
+	std::string name_string;
+	
+
 	ROS_DEBUG("Android ID: [%s]", msg->data.c_str());
+	
+	name = msg->data;
 
+	std::cout << name << std::endl;
+	name_string = name;
 
-	std::string angelika = "angelika";
-	std::string data = msg->data;
-	if (data.compare(angelika) == 0)
+	if (!prvNh.getParam(name_string, param_list)) 
 	{
-	std::cout << "perfekt" << std::endl; 
+	ROS_ERROR("failed to get data - check ID!");
 	}
-	else
-	{
-	std::cout << "other name" << std::endl;
-	}
+	else{
+	    ROS_INFO("param ok");
+	    }
 
+	std::cout << "testwert: " << param_list[0] << std::endl;
 
+	Target.header.frame_id = "map";	
+	Target.pose.position.x = param_list[0];
+	Target.pose.position.y = param_list[1];
+	Target.pose.orientation.w = param_list[2];
 
+	targetPos = nhServer.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
+	targetPos.publish(Target);
 
-
-	//targetPos.publish(Target);
+	ros::spin();
 }
 
 
@@ -42,88 +60,14 @@ int main(int argc, char** argv)
 
 	ros::init(argc, argv, "TargetPositionsNode");
 
-	ros::NodeHandle prvNh("~");
+	
 	ros::NodeHandle nhServer;
 
-	geometry_msgs::PoseStamped Target;
-
-
-
+	
 
 	ros::Subscriber sub = nhServer.subscribe("Android", 1, androidIDCallback);
 
-		//vektor-name hier variabel?! 
-	XmlRpc::XmlRpcValue ID_list;
-	XmlRpc::XmlRpcValue x_list;
-	XmlRpc::XmlRpcValue y_list;
-
-	if (!prvNh.getParam("id", ID_list)) 
-	{
-	ROS_ERROR("failed to get data - check ID!");
-	return(0);
-	}
-	else{
-	    ROS_INFO("ID-param ok");
-	    std::cout << "ID : " << ID_list[1] << std::endl;
-	    }
-
-	if(!prvNh.getParam("x", x_list)) 
-	{
-	ROS_ERROR("failed to get data - check x-positions!");
-	return(0);
-	}
-	else{
-	    ROS_INFO("x-param ok");
-	    std::cout << "x : " << x_list[0] << std::endl;
-	    }
-
-	if (!prvNh.getParam("y", y_list)) 
-	{
-   	ROS_ERROR("failed to get data - check y-positions!");
-	return(0);
-	}
-	else{
-	    ROS_INFO("y-param ok");
-	    std::cout << "y : " << x_list[0] << std::endl;
-	    }
-
-
-
-    	std::vector<std::string> persons;
-
-	for(int i=0; i < ID_list.size(); i++)
-	{
-		persons.push_back(ID_list[i]);
-	}	
-
 	
-/*
-		//"angelika" noch variabel gestalten!
-    	//int pos = std::find(ID_list.begin(), ID_list.end(), data "angelika") - ID_list.begin();
-    
-	int pos = std::find(ID_list.begin(), ID_list.end(), data "angelika") - ID_list.begin();
-    
-    	if(pos < ID_list.size())
-        	std::cout << "Position :" << pos << std::endl;
-    	else
-        	std::cout << "ID not found... please check" << std::endl;
-
-*/
-
-
-	Target.header.frame_id = "map";	
-	Target.pose.position.x = x_list[0];
-/*	Target.pose.position.y = y_list[pos];
-	Target.pose.orientation.w = 1;*/
-
-	//std::cout << "testParam2 :" << Target.pose.position.x << std::endl;
-	
-
-	ros::Publisher targetPos = nhServer.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
-
-
-	//publish
-	targetPos.publish(Target);
 
 	//ros::spin();
 
@@ -132,7 +76,7 @@ int main(int argc, char** argv)
 		ros::spinOnce();
 	}
 
-
+/*
 	ros::Rate rate(10); 
 	while(ros::ok())
 	{
@@ -141,5 +85,5 @@ int main(int argc, char** argv)
 		ros::spinOnce(); 
 		//rate.sleep(); 	
 	}
-		
+*/		
 }

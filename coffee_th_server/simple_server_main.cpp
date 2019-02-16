41737fdf80b60666f332b39700581e344536685e
moessneran <moessneran59696@th-nuernberg.de>
2019-02-16 13:18:21 +0100

    comments have been added

--- coffee_th_server/simple_server_main.cpp
+++ coffee_th_server/simple_server_main.cpp
@@ -6,6 +6,20 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
+/* 
+*	Implementation of the Simple_server_main class
+*	The simple_server_main.cpp file contains a ROS node. 
+*	All the names markes ros:: are ROS specific.
+*	@param: ros::init initializes a node with specific name
+*	@parma: ros:: NodeHandle creat a Ros node
+*	@parma: ros:: Publisher creat a publisher node which broadcast information
+*	@paramfunction: ServerSocket creat which listen to a specific port
+* 	@param: ServerSocket creat a new socket
+*	
+*	The message sent via the server client connetion is forwarded to the Ros accounts. 
+*	If the message does not contain an error, it will be interrupted.
+*/
+
 int main(int argc, char** argv)
 {
 	
